142b423cad390268e08874f71a823805dd6d0fb9
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2024-04-16 19:56:17 +0200

    add file

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import random
 
-myList = ['rock', 'paper', 'scissors']
-ran_num=random.randint(0,2)
-print(myList[ran_num])
+choices = ['rock', 'paper', 'scissors']
+computer = random.choice(choices)
+player=None
+while player not in choices:
+    player = input('Enter rock, paper, or scissors: ').lower()
+
+if player == computer:
+    print('Player:',player)
+    print('Computer',computer)
+    print('Draw')
+elif player == 'rock':
+    if computer == 'scissors':
+        print('Player:', player)
+        print('Computer', computer)
+        print('You win')
+    if computer == 'paper':
+        print('Player:', player)
+        print('Computer', computer)
+        print('You lose')
+elif player == 'scissors':
+    if computer == 'paper':
+        print('Player:', player)
+        print('Computer', computer)
+        print('You win')
+    if computer == 'rock':
+        print('Player:', player)
+        print('Computer', computer)
+        print('You lose')
+elif player == 'paper':
+    if computer == 'rock':
+        print('Player:', player)
+        print('Computer', computer)
+        print('You win')
+    if computer == 'scissors':
+        print('Player:', player)
+        print('Computer', computer)
+        print('You lose')
